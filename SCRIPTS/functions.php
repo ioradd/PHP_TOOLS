b7e8390993f452791b55570c3b45304252ad2380
ioradd <mlordsx1@gmail.com>
2023-03-17 12:17:47 +0100

    modif

--- SCRIPTS/functions.php
+++ SCRIPTS/functions.php
@@ -4,7 +4,7 @@ function connect_pdo_mysql() {
     $DATABASE_HOST = "localhost";
     $DATABASE_USER = "root"; 
     $DATABASE_PASS = "";
-    $DATABASE_NAME = "nomDeLaTable";
+    $DATABASE_NAME = "nomDeLaBdd";
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8' , $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
